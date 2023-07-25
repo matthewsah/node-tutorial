1210b50651f56e830223f727d01cf449371c7738
Matthew Sah <mattsah2001@gmail.com>
2023-07-25 13:47:12 -0700

    completed intro, moving on to HTTP

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,6 @@
 const { readFileSync, writeFileSync } = require('fs');
 
+console.log('starting task');
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
@@ -10,4 +11,7 @@ writeFileSync(
   './content/third.txt', 
   `hello :) this is the third text file: ${first}, ${second}`,
   { flag: 'a' }
-)
+);
+
+console.log('completed task');
+console.log('moving on to next task');
